23d76296381cfdd5c9f0182786f4b3d7e24219c1
Shaurya Bandari <162597024+Shauri12@users.noreply.github.com>
2025-10-23 11:22:07 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="Medical-Chatbot-using-Generative-AI",
+    version="0.1.0",
+    author="Team",
+    author_email="bandari170705@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    )
