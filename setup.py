ed1819e40e4f83898ce0336f982e077bbc8a9413
Shaurya Bandari <162597024+Shauri12@users.noreply.github.com>
2025-10-28 12:40:13 +0530

    Remove author email from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name="Medical-Chatbot-using-Generative-AI",
     version="0.1.0",
     author="Team",
-    author_email="bandari170705@gmail.com",
+    author_email=" ",
     packages=find_packages(),
     install_requires=[],
     )
